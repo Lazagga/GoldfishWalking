c8adf6dc9bb18b53da1735d1d4ef3bd9932a3cc2
Lazagga <81786003+Lazagga@users.noreply.github.com>
2025-12-21 03:32:25 +0900

    123

--- Assets/Scripts/Audio/PlayerData.cs
+++ Assets/Scripts/Audio/PlayerData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerData : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
